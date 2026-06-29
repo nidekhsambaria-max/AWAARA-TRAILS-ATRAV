182b7dc1f49ff23e7e6a772343178579c90eb7c9
max <nidekh.sambaria@gmail.com>
2026-06-29 14:32:08 +0530

    Add files via upload

---
+++ ai_studio_code.sh
@@ -0,0 +1,6 @@
+git init
+git add .
+git commit -m "Initial commit of AWAARA Trails"
+git branch -M main
+git remote add origin https://github.com/nidekhsambaria-max/AWAARA-TRAILS-ATRAV.git
+git push -u origin main
